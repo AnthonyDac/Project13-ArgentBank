22cd9dd43ff7b530fdbf052347803430036e0f0b
Daccarett Anthony <80802526+AnthonyDac@users.noreply.github.com>
2024-02-02 16:19:26 +0100

    protection sur le routeur vérification token

--- front/src/router/Routes.jsx
+++ front/src/router/Routes.jsx
@@ -1,10 +1,18 @@
 // Routes.js
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from '../pages/Homepage/HomePage';
 import NotFoundPage from '../pages/404/404Page';
 import SignInPage from '../pages/SignIn/SignInPage';
 import UserPage from '../pages/User/UserPage';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = ({ element }) => {
+  const token = useSelector((state) => state.auth.token);
+  const isAuthenticated = token && token.length ? true : false;
+
+  return isAuthenticated ? element : <Navigate to="/sign-in" />;
+};
 
 const Routers = () => {
   return (
@@ -12,7 +20,11 @@ const Routers = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/user" element={<UserPage />} />
+        <Route path="/sign-out" element={<SignInPage />} />
+        <Route
+          path="/user"
+          element={<PrivateRoute element={<UserPage />} />}
+        />
         <Route path="/404" element={<NotFoundPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
